4129b6bf9c01b761c946eedcdc7ec1ed93a402b0
Peter Matulis <peter.matulis@canonical.com>
2023-06-19 12:55:32 -0400

    Update intersphinx link to CDG for 2023.1
    Update to 2023.1 the intersphinx URL for the remote
    deploy-guide.
    
    Change-Id: I749695ecad94df7ddbc3da0b18f86cc7d0c77200

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -45,7 +45,7 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    'cdg': ('https://docs.openstack.org/project-deploy-guide/charm-deployment-guide/latest', None)
+    'cdg': ('https://docs.openstack.org/project-deploy-guide/charm-deployment-guide/2023.1', None)
 }
 
 # Spellchecker
